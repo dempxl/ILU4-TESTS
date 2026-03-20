65ffae84f9021d88de2fdb6bc3f28061f0e4d50e
dempxl <loganlarroux@gmail.com>
2026-03-20 19:12:56 +0100

    Implémentation RunTest

--- Java/src/classes/RunTest.java
+++ Java/src/classes/RunTest.java
@@ -2,9 +2,22 @@ package classes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileReader;
+
 import org.junit.jupiter.api.Test;
 
 public class RunTest {
 	// TODO lire le fichier txt regrouppant tout les test et en ressortir un fichier resultats.txt regrouppant tout les tests 
+	@Test
+	public static void run(FileReader file){
+		// TODO lis le fichier fr et execute les tests
+		
+		// TODO lis le fichier file
+
+		// TODO exécute les tests
+
+		// TODO ecrire les resultats dans un fichier resultats.txt
+		
+	}
 	
 }
